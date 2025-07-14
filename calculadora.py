dd3163042d0abdb6675bc21ba6f008bb416788f4
giselly em vocacao <giselly.nobre08@gmail.com>
2025-07-14 17:01:48 -0300

    atividade de logica

---
+++ calculadora.py
@@ -0,0 +1,29 @@
+##MVC
+#model
+###vai conter o resultado correspondente a conta
+
+#View
+###vai exibir o resultado na tela.
+
+#controller
+###
+
+
+def mostrar_menu():
+print("\n---calculadora---")
+print("1.  Adição")
+print("2.  Subtração")
+print("S.  sair")
+print("------------")
+
+ def obter_numeros():
+  while true:
+    try:
+num = float(input("Digite O primeiro número: "))
+num2 = float(input("Digite o segundo número: ")
+    return num1, num2
+
+       except ValueError:
+M print[" Entrada inválida. Por favor,
+"digite números válidos.")
+
