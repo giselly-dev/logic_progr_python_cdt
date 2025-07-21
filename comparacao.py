1b119cb474210b2e915187e7a45d3e90a17d09da
giselly em vocacao <giselly.nobre08@gmail.com>
2025-07-21 17:01:04 -0300

    Add files via upload
    atividade do dia

---
+++ comparacao.py
@@ -0,0 +1,49 @@
+### criando uma lista
+### tarso, gustavo,
+#  fabricio,vitor,
+#  gabriel, rafael, ivan
+
+
+#qual seria os print e impult para pedir os nomes?
+''
+print("*** lista de nomes  ***\n")
+
+nomes=  input ("digite os nomes separados por virgula: ").split(", ")
+ 
+ #nomes = [nome.stript() for nome in nomes]
+
+#print(nomes)
+
+
+print("\nInQuais operações você quer fazer:")
+print("1 Adicionar um nome")
+print("2 Remover um nome")
+print("3 Listar nomes")
+print("4 - Sair")
+
+#faça um loop para pedir a opção do usuario
+Wwhile True:
+  opcao= input ("\C "InDigite a opção desejada (1-4): ")
+
+
+- if opcao ==."1"
+
+#novo_nome-= -input("Digite o nome a ser-adicionado:")
+    -#nomes.append(novo_nome)
+     #print(f"(novo_nome) foi adicionado àlista.
+    #print(f" foi adicionado à lista.")
+
+    elif.оpcаo == "2":
+    
+        #nome_remover-=-input("Digite-o-nome-aser-removido:
+        #if-nome_remover in-nomes:
+#      nomes.remove(nome_remover)
+#       print(f"(nome_remover)-foi-removido-da lista.
+#  else:
+        #print(f"(nome_remover).não-está-na-lista."
+      print(f" não-está na lista.
+            
+      elif opcao == "3":
+    print("\nLista-de Nomes:")
+                #for-nome-in nomes:
+#            print(nome)
