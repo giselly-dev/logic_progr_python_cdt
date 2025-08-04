98b4105e060dd1b342cc5ec353552a156ede8a2d
giselly em vocacao <giselly.nobre08@gmail.com>
2025-08-04 16:51:30 -0300

    Add files via upload
    atividade feita com o professor em sala

---
+++ inventario_harry_potter.py
@@ -0,0 +1,30 @@
+    # Itens de poções
+    "Poção da Força": {
+        "quantidade": 2,
+        "descricao": "Aumenta temporariamente o poder dos feitiços.",
+        "tipo": "Poção"
+    },
+    # Itens de feitiços
+    "Feitiço Estupefaça": {
+        "quantidade": 1,
+        "descricao": "Feitiço de atordoamento que imobiliza o alvo.",
+        "tipo": "Feitiço"
+    },
+    # Itens de ingredientes
+    "Pó de Chifre de Bicornio": {
+        "quantidade": 5,
+        "descricao": "Ingrediente raro para poções complexas.",
+        "tipo": "Ingrediente"
+    },
+    # Itens de vestuário
+    "Capa da Grifinória": {
+        "quantidade": 1,
+        "descricao": "Uma capa de estudante com as cores da Grifinória.",
+        "tipo": "Vestuário"
+    },
+    # Itens de missão
+    "Chave de Prata": {
+        "quantidade": 1,
+        "descricao": "Uma chave misteriosa que abre uma porta secreta na Sala Precisa.",
+        "tipo": "Missão"
+    }
