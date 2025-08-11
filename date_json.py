b54fbd9599233b13846db9bc18e5b3afcde147b5
giselly em vocacao <giselly.nobre08@gmail.com>
2025-08-11 17:40:45 -0300

    Atividade em Sala

---
+++ date_json.py
@@ -0,0 +1,17 @@
+import json
+
+json_string = """[
+{
+"nome_moto" : " start 160 "
+"ano" : "2022/2023",
+"cor" : "preto",
+" seguro" : "sim"
+"marca" : "honda"
+"combustiveis" : [ "gasolina", "etanol"]
+},
+{
+
+}
+
+]
+"""
